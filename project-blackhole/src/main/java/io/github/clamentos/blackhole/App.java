f7c47faf91068357f7b9a5339b1954576cb9db42
Enrico <e.gattomonticone@gmail.com>
2023-06-02 19:14:13 +0200

    Getting the project started...

---
+++ project-blackhole/src/main/java/io/github/clamentos/blackhole/App.java
@@ -0,0 +1,12 @@
+package io.github.clamentos.blackhole;
+
+/**
+ * Main App class, empty for now...
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+}
